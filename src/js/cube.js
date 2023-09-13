b7f1843882570ea72ba900bc376b9753dc6991ae
hwajj <azamwara@gmail.com>
2023-09-13 15:12:59 +0900

    임시 커밋

--- src/js/cube.js
+++ src/js/cube.js
@@ -1,7 +1,10 @@
 import * as THREE from "three";
 import { OrbitControl } from "three/examples/jsm/controls/OrbitControls";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import Canvas from "./Canvas";
+// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+// import Canvas from "./Canvas";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import whale from '@/assets/Whale.glb'
+
 import html2canvas from "html2canvas";
 
 const scene = new THREE.Scene();
@@ -22,18 +25,29 @@ renderer.setSize(captureContainer.clientWidth, captureContainer.clientHeight);
 
 captureContainer.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
 
 camera.position.z = 5;
+
+const loader = new GLTFLoader();
+
+loader.load( whale, function ( gltf ) {
+  scene.add( gltf.scene );
+}, undefined, function ( error ) {
+  console.error( error );
+} );
+
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
 
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,3 @@
-import "@/css/style.css";
+import "@/css/style.scss";
 import "@/js/capture.js";
 import "@/js/cube.js";
